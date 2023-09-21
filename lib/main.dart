aabf34a33da45fb508d0d7a7dbbfc321b747f02c
Pedro <pedrosalesux@gmail.com>
2023-09-21 00:40:14 -0300

    Adicionou nova classe para fazer o decode e estruturação

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,3 @@
-import 'dart:convert';
+import 'package:challenge_flutter/networking.dart';
 
-import 'package:http/http.dart' as http;
-
-void main() => getData();
-
-Future getData() async {
-  http.Response response = await http.get(
-    Uri.parse('https://jsonplaceholder.typicode.com/posts'),
-  );
-  if (response.statusCode == 200) {
-    final post = jsonDecode(response.body);
-
-    return print(post);
-  } else {
-    print(response.statusCode);
-  }
-}
+void main() => getPosts();
--- lib/networking.dart
+++ lib/networking.dart
@@ -1,1 +1,41 @@
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+}
+
+Future<List<Post>> getPosts() async {
+  http.Response response = await http.get(
+    Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+  );
+  if (response.statusCode == 200) {
+    final decodedPosts = jsonDecode(response.body);
+    List<Post> posts = [];
+
+    for (dynamic post in decodedPosts) {
+      Post newPost = Post(
+          userId: post['userId'],
+          id: post['id'],
+          title: post['title'],
+          body: post['body']);
+      posts.add(newPost);
+    }
+
+    return posts;
+  } else {
+    print(response.statusCode);
+    return [];
+  }
+}
