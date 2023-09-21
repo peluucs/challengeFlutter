f2c45af1e112cb579225ccdf9dca979de77d7305
Pedro <pedrosalesux@gmail.com>
2023-09-21 01:45:48 -0300

    Exibição de titulos na tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,62 @@
 import 'package:challenge_flutter/networking.dart';
+import 'package:flutter/material.dart';
 
-void main() => getPosts();
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  late Future<List<Post>> posts;
+
+  @override
+  void initState() {
+    super.initState();
+    posts = getPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Text('Posts'),
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: posts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text('Carregando');
+          } else if (snapshot.hasError) {
+            return Text('Error');
+          } else if (snapshot.data!.isEmpty) {
+            return Text('Vazio');
+          } else {
+            print(snapshot.data!);
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index].title),
+                  //return Text(snapshot.data![1].title);
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
