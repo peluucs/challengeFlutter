2779736cfd21af58a2d7d7ef297c0c374ef0dcce
Pedro <pedrosalesux@gmail.com>
2023-09-22 00:21:04 -0300

    Finalização de requests e criação da interface

--- lib/details.dart
+++ lib/details.dart
@@ -1,17 +1,80 @@
 import 'package:challenge_flutter/networking.dart';
 import 'package:flutter/material.dart';
 
+String capitalizeFirstWord(String text) {
+  List<String> words = text.split(' ');
+  if (words.isNotEmpty) {
+    words[0] = words[0][0].toUpperCase() + words[0].substring(1);
+  }
+  return words.join(' ');
+}
+
 class Details extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Post post = ModalRoute.of(context)!.settings.arguments as Post;
+    getUser(post.userId);
     return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 249, 199, 1),
       appBar: AppBar(
-        backgroundColor: Colors.grey,
+        backgroundColor: Color.fromRGBO(34, 34, 34, 1),
         title: Text('Detalhes do Post'),
       ),
-      body: Center(
-        child: Text(post.body),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            alignment: AlignmentDirectional.topStart,
+            child: Text(
+              capitalizeFirstWord(post.title), // Capitaliza o título
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.all(16),
+            alignment: AlignmentDirectional.topStart,
+            child: Text(
+              capitalizeFirstWord(post.body), // Capitaliza o corpo do texto
+            ),
+          ),
+          Divider(
+            color: Colors.grey,
+            height: 1.0,
+            thickness: 1.0,
+            indent: 16.0,
+            endIndent: 16.0,
+          ),
+          SizedBox(height: 10),
+          FutureBuilder<User>(
+            future: getUser(post.userId),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+                //Loading
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error'));
+                //Erro
+              } else {
+                return Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(16),
+                      alignment: AlignmentDirectional.topStart,
+                      child: Text('Autor: ${snapshot.data!.username}'),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 16),
+                      alignment: AlignmentDirectional.topStart,
+                      child: Text('Email: ${snapshot.data!.email}'),
+                    ),
+                  ],
+                );
+              }
+            },
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:challenge_flutter/details.dart';
 import 'package:challenge_flutter/networking.dart';
 import 'package:flutter/material.dart';
 
-void main() => getUser(1); //runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -28,39 +28,63 @@ class HomeState extends State<Home> {
     posts = getPosts();
   }
 
+  String capitalizeFirstWord(String text) {
+    List<String> words = text.split(' ');
+    if (words.isNotEmpty) {
+      words[0] = words[0][0].toUpperCase() + words[0].substring(1);
+    }
+    return words.join(' ');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.fromRGBO(255, 249, 199, 1),
       appBar: AppBar(
-        backgroundColor: Colors.grey,
+        backgroundColor: Color.fromRGBO(34, 34, 34, 1),
         title: Text('Posts'),
       ),
       body: FutureBuilder<List<Post>>(
         future: posts,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-            //Loading
+            return Center(
+                child: CircularProgressIndicator(color: Colors.black));
           } else if (snapshot.hasError) {
             return Center(child: Text('Error'));
-            //Erro
           } else if (snapshot.data!.isEmpty) {
             return Center(child: Text('Vazio'));
-            //Empty
           } else {
-            print(snapshot.data!);
             return ListView.builder(
-              itemCount: snapshot.data!.length,
+              itemCount: snapshot.data!.length * 2 - 1,
               itemBuilder: (context, index) {
-                return ListTile(
-                  onTap: () {
-                    Navigator.pushNamed(context, '/details',
-                        arguments: snapshot.data![index]);
-                  },
-                  title: Text(snapshot.data![index].title),
-                  //return Text(snapshot.data![1].title);
-                );
+                if (index.isOdd) {
+                  return Divider(
+                    color: Colors.grey,
+                    height: 1.0,
+                    thickness: 1.0,
+                    indent: 16.0,
+                    endIndent: 16.0,
+                  );
+                } else {
+                  final itemIndex = index ~/ 2;
+                  final title =
+                      capitalizeFirstWord(snapshot.data![itemIndex].title);
+                  return ListTile(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/details',
+                          arguments: snapshot.data![itemIndex]);
+                    },
+                    title: Text(
+                      title,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  );
+                }
               },
             );
           }
